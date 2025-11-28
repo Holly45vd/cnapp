057bcc5f2b2796d0b9e29b2bad0a93f55c9ed136
Holly45vd <99254184+Holly45vd@users.noreply.github.com>
2025-11-28 04:48:34 +0000

    build: github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/cnapp/",   // 🔴 이거 중요
+  build: {
+    cssMinify: false, // 위에서 말한 우회책 쓰면 같이
+  },
+});
